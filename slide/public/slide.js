52f19ee6cc93f5d6c113b8542d343d70ba1da71b
syamsul muttaqin <lombok.oc@gmail.com>
2016-05-01 23:32:54 +0700

    masih error

--- slide/public/slide.js
+++ slide/public/slide.js
@@ -1,41 +1,174 @@
 'use strict';
 
+function Section() {
+
+	var _sectionElement 	= document.querySelector('section.present');
+	
+	this.getOriginalBgImage = function() {
+		return _sectionElement.dataset.background;
+	}
+
+	this.getBlurBgImage = function() {
+		var _splittedImageName 	= _sectionElement.dataset.background.split(new RegExp(/\.(jpe?g|png|gif)$/,'i'));
+		return _splittedImageName[0] + '-blur' + '.' + _splittedImageName[1];
+	}
+
+	this.getElement = function() {
+		return _sectionElement;
+	}
+
+	this.getDatasets = function() {
+		return _sectionElement.dataset;
+	}
+
+	this.getClasses = function() {
+		return _sectionElement.getAttribute('class');
+	}
+
+	this.isHaveClass = function(className) {
+		console.log(new RegExp(className,'i').test(this.getClasses()));
+		return new RegExp(/image-only/,'i').test(this.getClasses());
+	}
+
+	this.getStyle = function() {
+		return _sectionElement.getAttribute('style');
+	}
+
+	this.setStyle = function(style) {
+		_sectionElement.setAttribute('style', style);
+	}
+
+	this.isHavingDataset = function(datasetName) {
+		return _sectionElement.dataset.hasOwnProperty(datasetName);
+	}
+	
+	this.isHaveCustomBackgroundImage = function() {
+		return _sectionElement.dataset.hasOwnProperty('background');
+	}
+}
+
+function Slide(section) {
+
+	const _defCenterGradient	= .3,
+		  _defEdgeGradient 		= .6,
+		  _bgDivStyleArray 		= (function(){
+
+		  }());
+
+	function setStyle(centerGradientTransparency, edgeGradientTransprency) {
+
+		return  {
+			bgDiv: function() {
+		  		var div		= document.querySelector('div.slide-background.present'),
+					newStyle 		 	= `background:-webkit-radial-gradient(center, circle cover, rgba(250,250,250,`+ centerGradientTransparency + `) 0%, rgba(20,20,20,`+ edgeGradientTransprency +`) 100%),url('` + section.getOriginalBgImage() + `');
+			   							   background:-moz-radial-gradient(center, circle cover, rgba(250,250,250,`+ centerGradientTransparency + `) 0%, rgba(20,20,20,`+ edgeGradientTransprency +`) 100%),url('` + section.getOriginalBgImage() + `');
+			   							   background-size:cover;background-repeat:no-repeat;`;
+		  		
+
+		  		var originalDivStyle 	= div.getAttribute('style').split(/;\s*/);
+
+		  		originalDivStyle.forEach((item) => {
+
+		  			if ( !/background-image/.test(item) ) {
+		  				newStyle += item;
+		  			}
+
+		  		});
+
+				console.log(newStyle);
+			   	div.setAttribute('style', newStyle);
+				
+				return true;
+
+			}(),
+			section: function() {
+				section.setStyle(section.getStyle() + `background-image: -webkit-linear-gradient(0deg, rgba(255,255,255,.6), rgba(255,255,255,.6)), url('` + section.getBlurBgImage() + `');
+				      	background-image: -moz-linear-gradient(0deg, rgba(255,255,255,.6), rgba(255,255,255,.6)),url('` + section.getBlurBgImage() + `');
+				      	background-repeat: no-repeat;
+					    background-attachment: fixed;
+					    -moz-background-size: cover;
+					    -o-background-size: cover;
+					    -webkit-background-size: cover;
+					    background-size: cover;
+					    border-radius: 5px;
+					    border: 1px solid rgba(245,245,245,.4);
+					    box-shadow: 0 25px 37px rgba(10,20,30,1);
+					    padding-top: 0px;`);
+				return true;
+			}()
+		};
+	}
+
+	this.applyBackground = function() {
+		var centerGradientTransparency = _defCenterGradient,
+			edgeGradientTransprency = _defEdgeGradient;
+
+		if ( section.getElement().dataset.hasOwnProperty('backgroundTransparency') ) {
+
+			var transparency = section.getElement().dataset.backgroundTransparency.split(/\s+/);
+
+			if ( transparency.length > 1 && transparency.length < 3 ) {
+				centerGradientTransparency = typeof(transparency[0]) === 'number' ? transparency[0] : _defCenterGradient;
+				edgeGradientTransprency = typeof(transparency[1]) === 'number' ? transparency[1] : _defEdgeGradient;
+			} else {
+				centerGradientTransparency = _defCenterGradient;
+				edgeGradientTransprency = _defEdgeGradient;
+			}
+		}
+
+		setStyle(centerGradientTransparency, edgeGradientTransprency).bgDiv.section;
+	}
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
-	var Slide = (event) => {
-
+	function SSlide(event) {
 		
 		const _imagesPath 			= 'http://slide.seminar.sw/public/images/',
-			  _defBg 				= 'cover-bg-small.jpg',
-			  _defCenterGradient 	= .3,
-			  _defEdgeGradient 		= .6;
-		var	  _body 				= document.querySelector('body');
+			  _defBg 				= 'cover-bg-small.jpg';
+		var   _defCenterGradient 	= .3,
+			  _defEdgeGradient 		= .6,
+			  _backgroundDiv		= document.querySelector('div.slide-background.present');
 		
 		var _section = (new function() {
 
-			 	var section = document.querySelector('section.present');
-				var elementClasses = section.getAttribute('class');
+		  	var section = document.querySelector('section.present');
 
-				return {
-					element:section,
-					isContainClass: function(className) {
-						return new RegExp(className,'i').test(elementClasses);
-					},
-					getClasses: function () {
-						return elementClasses;
-					}
+			return {
+				element:section,
+				isHaveCustomBackgrund: function(){
+					return section.dataset.hasOwnProperty('background');
+				},
+				isContainClass: function(className) {
+					return new RegExp(className,'i').test(elementClasses);
+				},
+				getClasses: function () {
+					return section.getAttribute('class');
 				}
-			}());
+			}
+		}());
 
-		function getStyle(image, centerGradient, edgeGradient) {
+		function getStyle(centerGradientTransparency, edgeGradientTransprency) {
 
 			var imagePath = typeof(image) === 'undefined' ? getImagePath(_defBg) : getImagePath(image);
 
 			return  {
-				body: function() {
-					return `background:-webkit-radial-gradient(center, circle cover, rgba(250,250,250,`+ centerGradient + `) 0%, rgba(20,20,20,`+ edgeGradient +`) 100%),url('` + imagePath.original + `');
-				   			background:-moz-radial-gradient(center, circle cover, rgba(250,250,250,`+ centerGradient + `) 0%, rgba(20,20,20,`+ edgeGradient +`) 100%),url('` + imagePath.original + `');
-				   			background-size:cover;background-repeat:no-repeat;`;
+				divBackground: function() {
+
+					var originalDivStyle = _backgroundDiv.getAttribute('style').split(/;\s*/),
+						newStyle 		 =  `background:-webkit-radial-gradient(center, circle cover, rgba(250,250,250,`+ centerGradientTransparency + `) 0%, rgba(20,20,20,`+ edgeGradientTransprency +`) 100%),url('` + imagePath.original + `');
+				   							 background:-moz-radial-gradient(center, circle cover, rgba(250,250,250,`+ centerGradientTransparency + `) 0%, rgba(20,20,20,`+ edgeGradientTransprency +`) 100%),url('` + imagePath.original + `');
+				   							 background-size:cover;background-repeat:no-repeat;`;
+
+					originalDivStyle.forEach((item) => {
+						if ( /background-image/.test(item) ){
+							
+						}
+					});	
+
+
+
 				}(),
 				section: function() {
 					var originalSectionStyle = _section.element.getAttribute('style');
@@ -65,38 +198,10 @@ document.addEventListener('DOMContentLoaded', () => {
 			};
 		}
 	
-		function applyBackground(bgImage, centerGradient, edgeGradient) {
+		this.applyBackground = function() {
 
-			var bodyStyle,sectionStyle;
-
-			if ( _section.isContainClass('image-only') ) {
-			} else if ( _section.isContainClass('stacked-slide') ) {
-
-			}else {
-				bodyStyle = getStyle(bgImage, centerGradient, edgeGradient).body;
-				sectionStyle = getStyle(bgImage, centerGradient, edgeGradient).section;
-				_body.setAttribute('style', bodyStyle);
-				_section.element.setAttribute('style', sectionStyle);
-			}
-		}
-
-		var state = Reveal.getState();
-		switch( state.indexh ) {
-			case 4:
-				applyBackground('data-center-1.jpg',.2,.5);
-			break;
-			case 8:
-				applyBackground('html-source.jpg',.3,.6);
-			break;
-			case 12:
-				applyBackground('student-in-lib.jpg',.3,.6);
-			break;
-			default:
-				applyBackground();
-			break;
-		}
+		};
 	};
-
 
 	Reveal.initialize({
 		transition:'convex',
@@ -106,6 +211,12 @@ document.addEventListener('DOMContentLoaded', () => {
 		progress: false
 	});
 
-	Reveal.addEventListener('slidechanged', Slide);
+	Reveal.addEventListener('slidechanged', () => {
+		var x = new Section;
+		if ( !x.isHaveClass('(image-only|stacked-slide)') && x.isHaveCustomBackgroundImage() ) {
+			var slide = new Slide(x);
+			slide.applyBackground();
+		}
+	});
 
 });
